fd609709ecaa37a5582ad17b6d58e70dd0062050
guiljin <03jsjjgl@sohu.com>
2019-08-20 12:51:27 +0800

    remove folder myPython

--- myPython/test3.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-proxies = {
-    "http": "http://10.144.1.10:8080",
-    "https": "https://10.144.1.10:8080"
-}
-r = requests.post("http://httpbin.org/post", proxies=proxies)
-print r.text
